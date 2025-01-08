935cdf8341ddaa6d598c3fab057f108a7846cb9a
pacmanninja998 <pacmanninja998@gmail.com>
2025-01-08 18:05:09 -0500

    More challenges

---
+++ karen's Projects/Numbers/Fizz Buzz.py
@@ -0,0 +1,12 @@
+def main ():
+	for n in range(101):
+		if n % 3 == 0 and n % 5 == 0:
+			print("FizzBuzz")
+		elif n % 3 == 0:
+			print("Fizz")
+		elif n % 5 == 0:
+			print("Buzz")
+		else:
+			print(n)
+if __name__ == "__main__":
+    main()
